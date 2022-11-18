d07ea035e3c196c9e08fb700b091dcffd4357452
sheikh <skuddin.ius@gmail.com>
2022-11-18 14:04:42 +0600

    added theme development file

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+if(have_posts()):
+    while(have_posts()):
+        the_post();?>
+            <h1><?php the_title()?></h1>
+            <p><?php the_content() ?></p>
+        <?php
+    endwhile;
+endif; ?>
